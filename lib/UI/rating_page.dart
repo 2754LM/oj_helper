947176b01d8c40fe3a62d016d0c397455e6aba97
kano <2754305745@qq.com>
2024-07-10 01:06:23 +0800

    style:清除无用库

--- lib/UI/rating_page.dart
+++ lib/UI/rating_page.dart
@@ -1,4 +1,3 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:oj_helper/models/rating.dart';
 import 'package:oj_helper/ui/widgets/platform_help.dart';
